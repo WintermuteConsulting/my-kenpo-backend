de5e90ec9e6bf11f91629e3167135b30fb4831f9
Ben Brunk <brunkb@gmail.com>
2017-04-22 19:45:40 -0500

    Routing static content, added a json sample.

--- src/main/groovy/com/mykenpo/KenpoApplication.groovy
+++ src/main/groovy/com/mykenpo/KenpoApplication.groovy
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableWebMvc
 class KenpoApplication {
 
     static void main(String[] args) {
